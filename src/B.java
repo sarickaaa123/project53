cf95a3b5aeee82d970fd89d1b32fe65657185c19
Sara Usevska <sarausevska42@yahoo.com>
2024-02-11 22:48:57 +0100

    first commit

---
+++ src/B.java
@@ -0,0 +1,6 @@
+
+public class B { // SUPERCLASS B
+	int x=2; // DATA MEMBER SUPERCLASS x = 2
+	void triple() {x=x*3;} // metod - pomnozi ja vrednosta x so 3 x*=3
+	int returnIt() {return x;} // metod - vrati go podatokot-DATA MEMBER x
+	}
---
+++ src/C.java
@@ -0,0 +1,6 @@
+
+	public class C extends B { // SUBCLASS C
+		void triple() {
+		x=x+3; // metod - Zgolemi ja vrednosta na x so 3 x+=3
+		}
+	}
